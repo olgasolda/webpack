1b8d26f5577588f24f1c39f821afbb503651f7e8
olgasolda <olyasis@gmail.com>
2023-05-16 19:08:59 +0300

    css minimizer

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,11 +1,15 @@
-import './css/style.css';
+import * as $ from 'jquery';
 
 import Post from "./post";
 import json from './data.json';
-import xml from './data.xml';
 import logo from './webpack.png';
+import csv from './data.csv';
+
+import '@/css/style.css';
 
 const post = new Post('Webpack Post Title', logo);
 
-console.log('Post to string:', post.toString());
+// console.log('Post to string:', post.toString());
+$('pre').addClass('code').html(post.toString());
 console.log('JSON', json);
+console.log('CSV', csv);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+console.log('IS DEV:', isDev);
+console.log('IS PROD:', isProd);
 
 module.exports = {
   context: path.resolve(__dirname, 'src/assets'),
@@ -11,19 +19,52 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
+    filename: '[name].[fullhash].js',
   },
+  target: 'web',
+  devServer: {
+    hot: false,
+    port: 4200
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.ts', '.tsx', '.jsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src/assets'),
+    }
+  },
+  optimization: {
+    splitChunks: {
+    chunks: 'all'
+    },
+    minimizer: [
+      new CssMinimizerWebpackPlugin()
+    ]
+    },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html'
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+      {
+      from: path.resolve(__dirname, 'src/assets/favicon.png'),
+      to: path.resolve(__dirname, 'dist'),
+      }
+      ]
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].[fullhash].css'
+        })
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [
+          {loader: MiniCssExtractPlugin.loader},
+          'css-loader'
+        ]
       },
       {
         test: /\.(png|jpg|jpeg|webp|gif|svg)$/,
@@ -35,15 +76,18 @@ module.exports = {
       {
         test: /\.(woff|woff2|ttf|eot)$/,
         type: 'asset/resource',
-        generator: {
-          filename: 'assets/fonts/[name].[hash][ext]'
-        }
+        // generator: {
+        //   filename: 'assets/fonts/[name].[hash][ext]'
+        // }
       },
       {
         test: /\.xml$/,
         use: ['xml-loader']
-        }
-      }
-    ]
+        },
+        {
+          test: /\.csv$/,
+          use: ['csv-loader']
+          }
+   ]
   }
 };
